83afb9a28e4ee2287e60da263fbe97db0f7927d2
Kimberley09 <kimberleyc8@nycstudents.net>
2025-03-25 22:51:26 -0400

    asd

--- open-data/src/router/index.js
+++ open-data/src/router/index.js
@@ -4,12 +4,12 @@ import FilterView from '../views/FilterView.vue';
 
 const routes = [
   { path: '/', name: 'home', component: HomeView },
-  { path: '/filter', name: 'filter', component: FilterView }
+  { path: '/filter', name: 'filter', component: FilterView },
 ];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes
+  routes,
 });
 
 export default router;
